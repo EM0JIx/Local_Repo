9c8c77eb7a133e82b2e2136c2d7a3b0540b40994
EM0JIx <L0n3Wol421@gmail.com>
2024-09-15 13:26:49 +0530

    Added Intial Files
    Added an Html, Css & Js file
    
    Name of the 3 files are: Test1.html, Style.css, Index.js

---
+++ Index.js
@@ -0,0 +1,9 @@
+// Wait for the DOM to fully load
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the h1 element and change its text
+  const heading = document.querySelector("h1");
+  heading.textContent = "Welcome to My Website!";
+
+  // Log a message to the console
+  console.log("JavaScript is connected and running!");
+});
